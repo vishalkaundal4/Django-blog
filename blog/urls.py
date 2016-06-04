e116f405d1e8c5d5e6753cbb5128aa7544acd2ef
vishal <vishal.kaundal4@gmail.com>
2016-06-04 16:39:32 +0530

    Django Blog ,first commit

---
+++ blog/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import url
+import views
+urlpatterns=[url(r'^$',views.post_list,name='post_list'),url(r'^post/new$',views.post_new,name='post_new')]
---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from models import Post
+from forms import PostForm
+from django.utils import timezone
+# Create your views here.
+def post_list(request):
+	posts=Post.objects.all()
+	return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_new(request):
+	if request.method=='POST':
+		form=PostForm(request.POST)
+		if form.is_valid():
+			post=form.save(commit=False)
+			post.author=request.user
+			post.published_date=timezone.now()
+			post.save()
+			return redirect('post_list')
+	else:
+		form=PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
